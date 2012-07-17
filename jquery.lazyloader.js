da426d86eb429ef7f7e58d57b2438ce98853884c
daniel <daniel@promethost.com>
2012-07-17 20:13:21 +0800

    added wait icons

--- jquery.lazyloader.js
+++ jquery.lazyloader.js
@@ -14,7 +14,10 @@
   $.fn.lazyloader = function(options){
     var settings = $.extend($.fn.lazyloader.defaults, options);
     var images = this;
-             
+    
+    // add the wait icon
+    $('img[actual-src]').parent().css({ position: 'relative', display: 'block'}).prepend('<img class="lazyloader-wait" src="' + settings['icon'] + '" />');
+                     
     // Load on refresh
     loadActualImages(images, settings);
 
@@ -27,30 +30,40 @@
   };
     
   // Defaults
-  $.fn.lazyloader.defaults = {distance: 0};
+  $.fn.lazyloader.defaults = { 
+    distance: 0, // the distance (in pixels) of image when loading of the actual image will happen
+    icon: ''     // display animating icon
+  };
  
  
   // Loading actual images
-  function loadActualImages(images, options){
+  function loadActualImages(images, settings){
     images.each(function(){
-      if (windowView(this, options) && ($(this).attr('actual-src'))){
+      var imageHeight = $(this).height(), imageWidth = $(this).width();
+      var waitTop = Math.round(imageHeight/2), waitLeft = Math.round(imageWidth/2), waitFactor = Math.round($(this).siblings('.lazyloader-wait').height()/2);
+      $(this).siblings('.lazyloader-wait').css({ top: waitTop - waitFactor, left: waitLeft - waitFactor });
+      
+      if (windowView(this, settings) && ($(this).attr('actual-src'))){
         loadImage(this);
+        $(this).fadeIn('slow');
       }
-      $(this).fadeIn('slow');
     });
   };
         
     
   // Check if the images are within the window view
-  function windowView(image, options){
+  function windowView(image, settings){
     var view = $(window).height() + $(window).scrollTop(), image_pos = $(image).offset();     
-    return view >= (image_pos.top - options['distance']);
+    return view >= (image_pos.top - settings['distance']);
   };
     
     
   // Load the image
   function loadImage(image){
-    $(image).attr('src', $(image).attr('actual-src')).removeAttr('actual-src');
+    $(image).hide().attr('src', $(image).attr('actual-src')).removeAttr('actual-src');
+    $(image).load(function(){
+      $(this).siblings('img.lazyloader-wait').remove();
+    });
   };
      
 })(jQuery);
