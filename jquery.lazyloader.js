265aeccbdb97071dd4255bdf7d30ad991d60486a
joelpittet <joelpittet@160302.no-reply.drupal.org>
2015-03-17 15:23:46 +0100

    Issue #2453501 by joelpittet: DOM lookup improvements

--- jquery.lazyloader.js
+++ jquery.lazyloader.js
@@ -12,10 +12,17 @@
 
 (function($){
 
+  // Window jQuery object global reference.
+  var $window;
+
   // Process lazyloader
   $.fn.lazyloader = function(options){
     var settings = $.extend($.fn.lazyloader.defaults, options);
     var images = this;
+
+    if (typeof($window) == 'undefined') {
+      $window = $(window);
+    }
 
     // add the loader icon
     if(settings['icon'] != '') $('img[data-src]').parent().css({ position: 'relative', display: 'block'}).prepend('<img class="lazyloader-icon" src="' + settings['icon'] + '" />');
@@ -24,12 +31,12 @@
     loadActualImages(images, settings);
 
     // Load on scroll
-    $(window).bind('scroll', function(e){
+    $window.bind('scroll', function(e) {
       loadActualImages(images, settings);
     });
 
     // Load on resize
-    $(window).resize(function(e){
+    $window.resize(function(e) {
       loadActualImages(images, settings);
     });
 
@@ -49,13 +56,14 @@
 
     $.fn.lazyloader.timeout = setTimeout(function(){
       images.each(function(){
-        var imageHeight = $(this).height(), imageWidth = $(this).width();
-        var iconTop = Math.round(imageHeight/2), iconLeft = Math.round(imageWidth/2), iconFactor = Math.round($(this).siblings('img.lazyloader-icon').height()/2);
-        $(this).siblings('img.lazyloader-icon').css({ top: iconTop - iconFactor, left: iconLeft - iconFactor });
+        var $image = $(this);
+        var imageHeight = $image.height(), imageWidth = $image.width();
+        var iconTop = Math.round(imageHeight/2), iconLeft = Math.round(imageWidth/2), iconFactor = Math.round($image.siblings('img.lazyloader-icon').height()/2);
+        $image.siblings('img.lazyloader-icon').css({ top: iconTop - iconFactor, left: iconLeft - iconFactor });
 
-        if (windowView(this, settings) && ($(this).attr('data-src'))){
+        if (windowView(this, settings) && ($image.attr('data-src'))) {
           loadImage(this);
-          $(this).fadeIn('slow');
+          $image.fadeIn('slow');
         }
       });
     }, 50);
@@ -64,23 +72,23 @@
 
   // Check if the images are within the window view (top, bottom, left and right)
   function windowView(image, settings){
+    var $image = $(image);
+    // window variables
+    var windowHeight = $window.height(),
+        windowWidth  = $window.width(),
 
-        // window variables
-    var windowHeight = $(window).height(),
-        windowWidth  = $(window).width(),
-
-        windowBottom = windowHeight + $(window).scrollTop(),
+        windowBottom = windowHeight + $window.scrollTop(),
         windowTop    = windowBottom - windowHeight,
-        windowRight  = windowWidth + $(window).scrollLeft(),
+        windowRight  = windowWidth + $window.scrollLeft(),
         windowLeft   = windowRight - windowWidth,
 
         // image variables
-        imageHeight  = $(image).height(),
-        imageWidth   = $(image).width(),
+        imageHeight  = $image.height(),
+        imageWidth   = $image.width(),
 
-        imageTop     = $(image).offset().top - settings['distance'],
+        imageTop     = $image.offset().top - settings['distance'],
         imageBottom  = imageTop + imageHeight + settings['distance'],
-        imageLeft    = $(image).offset().left - settings['distance'],
+        imageLeft    = $image.offset().left - settings['distance'],
         imageRight   = imageLeft + imageWidth + settings['distance'];
 
            // This will return true if any corner of the image is within the screen
@@ -91,9 +99,10 @@
 
   // Load the image
   function loadImage(image){
-    $(image).hide().attr('src', $(image).data('src')).removeAttr('data-src');
-    $(image).load(function(){
-      $(this).siblings('img.lazyloader-icon').remove();
+    var $image = $(image);
+    $image.hide().attr('src', $image.data('src')).removeAttr('data-src');
+    $image.load(function() {
+      $image.siblings('img.lazyloader-icon').remove();
     });
   };
 
